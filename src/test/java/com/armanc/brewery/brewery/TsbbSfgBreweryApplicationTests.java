e5446ee104997f4b04e7cab146e183a3a9af72cd
Armanç Tataroğlu <atataroglu@gmail.com>
2022-02-15 02:14:07 +0300

    init

---
+++ src/test/java/com/armanc/brewery/brewery/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armanc.brewery.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbbSfgBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
